ecc54a6578135f465a1d6f224669d2ea45665644
Edoardo Morandi <morandidodo@gmail.com>
2024-10-29 22:52:27 +0100

    Remove incomplete parts of lending iterators

--- src/lending_iterator.rs
+++ src/lending_iterator.rs
@@ -4,41 +4,4 @@ pub trait LendingIterator {
         Self: 'a;
 
     fn next(&mut self) -> Option<Self::Item<'_>>;
-
-    fn filter<P>(self, predicate: P) -> Filter<Self, P>
-    where
-        Self: Sized,
-        for<'a> P: FnMut(&Self::Item<'a>) -> bool,
-    {
-        Filter {
-            iter: self,
-            predicate,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Filter<I, P> {
-    iter: I,
-    predicate: P,
-}
-
-impl<I, P> LendingIterator for Filter<I, P>
-where
-    I: LendingIterator,
-    for<'a> P: FnMut(&I::Item<'a>) -> bool,
-{
-    type Item<'a> = I::Item<'a>
-    where
-        Self: 'a;
-
-    fn next(&mut self) -> Option<Self::Item<'_>> {
-        // while let Some(element) = self.iter.next() {
-        //     if (self.predicate)(&element) {
-        //         return Some(element);
-        //     }
-        // }
-
-        None
-    }
 }
